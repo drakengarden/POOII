9ed507acffa3550f4494989873316eb9869d0352
drakengarden <jileon@uamv.edu.ni>
2023-03-23 18:48:47 -0600

    JPA Implemented

--- classJPA/classJPA/src/main/java/dao/IDAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-import java.util.List;
-
-public interface IDAO { // crud
-    <T> List<T> findAll(Class clazz); // class unknown; type unknown (T)
-    <T> void save(T entity);
-    <T> void delete(T entity);
-}
