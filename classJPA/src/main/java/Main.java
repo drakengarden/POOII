28777c78720ee5ee83a9bc1fa08fd886e01e127c
drakengarden <jileon@uamv.edu.ni>
2023-03-27 18:34:52 -0600

    Student Implemented

--- classJPA/src/main/java/Main.java
+++ classJPA/src/main/java/Main.java
@@ -1,15 +1,21 @@
 import dao.IDAO;
 import dao.ImpDAO;
 import model.Author;
+import model.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author(1L, "3"); // initialize instance of author class
+        /*Author author = new Author(1L, "3"); // initialize instance of author class
         IDAO dao = new ImpDAO(); // initialize dao instance of impdao class to use crud
         //Author author1 = dao.findById(author.getId(), Author.class);
         dao.create(author);
         //dao.update(author);
         //dao.delete(author1);
-        System.out.println(dao.findAll(Author.class, "Author.all"));
+        System.out.println(dao.findAll(Author.class, "Author.all"));*/
+
+        Student a = new Student(null, "A", "B");
+        IDAO dao = new ImpDAO();
+        dao.create(a);
+        System.out.println(dao.findAll(Student.class, "Student.all"));
     }
 }
