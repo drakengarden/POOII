f579c39bd605205166c22d5e772674bdfcd2a6ff
Javier León <jileon@uamv.edu.ni>
2023-05-27 17:48:48 -0600

    Delete Downloads/PhysicsEngineV_0_1/PhysicsEngineV_0_1 directory

--- Downloads/PhysicsEngineV_0_1/PhysicsEngineV_0_1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.physicsenginev_0_1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.physicsenginev_0_1 to javafx.fxml;
-    exports com.example.physicsenginev_0_1;
-}
